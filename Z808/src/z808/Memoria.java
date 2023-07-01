1336853c130a54350019269b50e04e469739fdd8
Maria Júlia <105013681+majudlorenzoni@users.noreply.github.com>
2023-07-01 17:25:05 -0300

    Update Memoria.java
    Tamanho da memória

--- Z808/src/z808/Memoria.java
+++ Z808/src/z808/Memoria.java
@@ -5,5 +5,22 @@
 package z808;
 
 public class Memoria {
-    // Aqui o código referente a manipulação da memória
-}
+    private static final int TAMANHO_MAXIMO = 64 * 1024; // Tamanho da memória 64K
+
+    private int[] dados;
+    private int[] codigo;
+    private int[] pilha;
+
+    public Memoria (int tamanhoDados, int tamanhoCodigo, int tamanhoPilha) {
+        int tamanhoTotal = tamanhoDados + tamanhoCodigo + tamanhoPilha;
+        
+        // Impede que os três segmentos juntos não ultrapassem 64 K
+        if (tamanhoTotal > TAMANHO_MAXIMO) { 
+            throw new IllegalArgumentException("O tamanho total dos segmentos excede o limite máximo de 64K");
+        }
+        
+        dados = new int[tamanhoDados];
+        codigo = new int[tamanhoCodigo];
+        pilha = new int[tamanhoPilha];
+    }
+
