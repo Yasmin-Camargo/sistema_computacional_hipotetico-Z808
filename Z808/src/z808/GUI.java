9fcb23ada4f0bb36fa94cc54579d83eefd22181c
Claudio J. <48624347+CloudioJ@users.noreply.github.com>
2023-07-02 16:04:50 -0300

    GUI em processo

--- Z808/src/z808/GUI.java
+++ Z808/src/z808/GUI.java
@@ -5,13 +5,16 @@
 package z808;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
@@ -28,18 +31,34 @@ public class GUI extends JFrame{
 		
 		//Cria a janela em x por y
 		JFrame frame = new JFrame("Z808 Emulator");
+		frame.setBackground(Color.GRAY);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 600);
+		
+		JTextArea instrucoes = new JTextArea();
 
 		//Cria o painel de seleção de arquivo fonte
+		JPanel painel_principal = new JPanel();
 		JPanel painel_selecao = new JPanel();
+		JPanel painel_instrucoes = new JPanel();
+		
+		painel_principal.setBounds(560, 55, 200, 450);
+		painel_principal.setBorder(BorderFactory.createLineBorder(Color.BLUE, 3));
+		
+		painel_instrucoes.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
+
+		instrucoes.setBounds(10,30,50,250);
+		painel_principal.add(instrucoes);
 		JLabel label = new JLabel("Arquivo fonte:");
+		label.setForeground(Color.white);
 		JButton escolher_arquivo = new JButton("...");
 		JButton confirmar = new JButton ("Confirmar");
+		
 		painel_selecao.add(label);
 		painel_selecao.add(barra_texto);
 		painel_selecao.add(escolher_arquivo);
 		painel_selecao.add(confirmar);
+		painel_principal.add(new JLabel());
 		
 		//Seleciona o arquivo pra virar a fonte
 		escolher_arquivo.addActionListener((ActionEvent e) -> {
@@ -60,8 +79,12 @@ public class GUI extends JFrame{
 			nome_arquivo = barra_texto.getText();
 		});
 		
+		painel_principal.setBackground(Color.red);
+		painel_selecao.setBackground(Color.black);
 		//Posiciona painel de seleção no topo
-		frame.getContentPane().add(BorderLayout.NORTH, painel_selecao);
+//		frame.getContentPane().add(BorderLayout.EAST, painel_visual);
+		frame.add(painel_principal);
+		frame.add( painel_selecao);
 		frame.setVisible(true);
 		
 	}
